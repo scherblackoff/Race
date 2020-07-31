062c6b58a9fedcbca4d12d7c5f4671a610a723c5
scherblackoff <scherblackoff@gmail.com>
2020-07-31 15:45:30 +0300

    homework

---
+++ src/java3/Stage.java
@@ -0,0 +1,13 @@
+package java3;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
+}
+
